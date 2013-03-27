c293343cfeade63d598d889ffff6507da811af01
Kausikram Krishnasayee <kausikram@gmail.com>
2013-03-27 11:34:59 +0530

    added the database sql to the repo

---
+++ resources/datastructure.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS `profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `full_name` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile_properties`
+--
+
+CREATE TABLE IF NOT EXISTS `profile_properties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `profile_id` int(11) NOT NULL,
+  `key` text NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=202 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile_psych`
+--
+
+CREATE TABLE IF NOT EXISTS `profile_psych` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `profile_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `observation` text NOT NULL,
+  `action_points` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
